2feacafa9cefb8ab075178e6a2399697d1ccb4e3
Javier Ramos Nodal <javiramosnodal@gmail.com>
2022-10-30 09:31:09 +0100

    Lab.060.

---
+++ Lab.060-Ejemplo de pruebas de código únicas/src/DEMO-Pruebas de código únicas/UtilidadesMatematicas.cs
@@ -0,0 +1,27 @@
+﻿namespace DEMO_Pruebas_de_código_únicas
+{
+    public static class UtilidadesMatematicas
+    {
+        public static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static bool EsPar(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Lab.060-Ejemplo de pruebas de código únicas/test/UtilidadesMatematicasTests/UtilidadesMatematicasTests.cs
@@ -0,0 +1,80 @@
+﻿using DEMO_Pruebas_de_código_únicas;
+using System;
+using Xunit;
+
+namespace UtilidadesMatematicasTests
+{
+    public class UtilidadesMatematicasTests
+    {
+        [Fact]
+        public void Sumar_ShouldBe5_IfA3AndB2()
+        {
+            //Arrange
+            var a = 3;
+            var b = 2;
+
+            //Act
+            var resultado = UtilidadesMatematicas.Sumar(a, b);
+
+            //Assert
+            Assert.Equal(5, resultado);
+        }
+
+        [Fact]
+        public void EsPar_ShouldBeTrue_IfA2()
+        {
+            //Arrange
+            var a = 2;
+
+            //Act
+            var resultado = UtilidadesMatematicas.EsPar(a);
+
+            //Assert
+            Assert.True(resultado);
+        }
+
+        [Fact]
+        public void EsPar_ShouldBeFalse_IfA1()
+        {
+            //Arrange
+            var a = 1;
+
+            //Act
+            var resultado = UtilidadesMatematicas.EsPar(a);
+
+            //Assert
+            Assert.False(resultado);
+        }
+
+        [Fact]
+        public void Dividir_ShouldBe4_IfA8AndB2()
+        {
+            //Arrange
+            var a = 8;
+            var b = 2;
+
+            //Act
+            var resultado = UtilidadesMatematicas.Dividir(a, b);
+
+            //Assert
+            Assert.Equal(4, resultado);
+        }
+
+        [Fact]
+        public void Dividir_ShouldThrowDivideByZeroException_IfA8AndB0()
+        {
+            //Arrange
+            var a = 8;
+            var b = 0;
+
+            //Act
+            Action act = () =>
+            {
+                var resultado = UtilidadesMatematicas.Dividir(a, b);
+            };
+
+            //Assert
+            Assert.Throws<DivideByZeroException>(act);
+        }
+    }
+}
