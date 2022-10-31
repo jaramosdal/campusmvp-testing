81a6336d52a64637029c6fc1efe9016b71548c7c
Javier Ramos Nodal <javiramos@soliss.com>
2022-10-31 11:36:17 +0100

    Demo teorías con InlineData.

---
+++ 03_Demo_Teorias/2-test/FactoriaDeMensajesTests/FactoríaDeMensajesTests.cs
@@ -0,0 +1,25 @@
+using Demo_Teorias;
+
+namespace FactoriaDeMensajesTests;
+
+public class FactoríaDeMensajesTests
+{
+    [Theory]
+    [InlineData("Hola", 0)]
+    [InlineData("Adiós", 1)]
+    [InlineData("CampusMVP", 2)]
+    [InlineData("Pruebas", 3)]
+    [InlineData("De", 4)]
+    [InlineData("Código", 5)]
+    [InlineData("No se ha podido recuperar", 6)]
+    public void ObtenerMensaje_ShouldBeCorrectMessage(string expected, int code)
+    {
+        //Arrange
+
+        //Act
+        var resultado = FactoríaDeMensajes.ObtenerMensaje(code);
+
+        //Assert
+        Assert.Equal(expected, resultado);
+    }
+}
