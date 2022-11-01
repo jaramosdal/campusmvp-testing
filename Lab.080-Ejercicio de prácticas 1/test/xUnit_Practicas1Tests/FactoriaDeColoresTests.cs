54ec869f8b83d46fe69bd44103b741218ed3dadc
Javier Ramos Nodal <javiramos@soliss.com>
2022-11-01 21:15:42 +0100

    Pruebas de colores con ARGB no válidos.

--- Lab.080-Ejercicio de prácticas 1/test/xUnit_Practicas1Tests/FactoriaDeColoresTests.cs
+++ Lab.080-Ejercicio de prácticas 1/test/xUnit_Practicas1Tests/FactoriaDeColoresTests.cs
@@ -1,13 +1,19 @@
 using System.Drawing;
 using xUnit_Practicas1;
+using xUnit_Practicas1Tests.InputTestsData;
 
 namespace xUnit_Practicas1Tests;
 
 public class FactoriaDeColoresTests
 {
     [Theory]
-    [InlineData(Color.Red, "rojo")]
-    public void GetColorByName_ShouldBeCorrectColor(Color expected, string colorName)
+    [InlineData("Red", "rojo")]
+    [InlineData("Blue", "azul")]
+    [InlineData("Green", "verde")]
+    [InlineData("White", "blanco")]
+    [InlineData("Black", "negro")]
+    [InlineData("0", "something")]
+    public void GetColorByName_ShouldBeCorrectColor(string expected, string colorName)
     {
         //Arrange
         // string colorName = "";
@@ -16,6 +22,54 @@ public class FactoriaDeColoresTests
         var color = FactoriaDeColores.GetColorByName(colorName);
 
         //Assert
-        Assert.Equal(expected, color);
+        Assert.Equal(expected, color.Name);
+    }
+
+    //[Theory]
+    //[ClassData(typeof(FactoriaDeColoresTestsData))]
+    //public void GetColorComposition_ShouldThrowArgumentOutOfRangeException_IfAnyNotIn0To255(int red, int green, int blue)
+    //{
+    //    //Arrange
+    //    // string colorName = "";
+
+    //    //Act
+    //    Action act = () =>
+    //    {
+    //        var resultado = FactoriaDeColores.GetColorComposition(red, green, blue);
+    //    };
+
+    //    //Assert
+    //    Assert.Throws<ArgumentOutOfRangeException>(act);
+    //}
+
+    [Theory]
+    [MemberData(nameof(FactoriaDeColoresTestsData.InvalidData), MemberType = typeof(FactoriaDeColoresTestsData))]
+    public void GetColorComposition_ShouldThrowArgumentOutOfRangeException_IfAnyNotIn0To255(int red, int green, int blue)
+    {
+        //Arrange
+        // string colorName = "";
+
+        //Act
+        Action act = () =>
+        {
+            var resultado = FactoriaDeColores.GetColorComposition(red, green, blue);
+        };
+
+        //Assert
+        Assert.Throws<ArgumentOutOfRangeException>(act);
+    }
+
+    [Theory]
+    [MemberData(nameof(FactoriaDeColoresTestsData.ValidData), MemberType = typeof(FactoriaDeColoresTestsData))]
+    public void GetColorComposition_ShouldBeCorrectColor_IfAllIn0To255(int red, int green, int blue)
+    {
+        //Arrange
+        // string colorName = "";
+
+        //Act
+        var color = FactoriaDeColores.GetColorComposition(red, green, blue);
+
+        //Assert
+        // TODO
     }
 }
---
+++ Lab.080-Ejercicio de prácticas 1/test/xUnit_Practicas1Tests/InputTestsData/FactoriaDeColoresTestsData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnit_Practicas1Tests.InputTestsData;
+
+//public class FactoriaDeColoresTestsData : TheoryData<int, int, int>
+//{
+//    public FactoriaDeColoresTestsData()
+//    {
+//        Add(-1, 0, 0);
+//        Add(256, 0, 0);
+//        Add(0, -1, 0);
+//        Add(0, 256, 0);
+//        Add(0, 0, -1);
+//        Add(0, 0, 256);
+//    }
+//}
+
+public class FactoriaDeColoresTestsData 
+{
+    public static IEnumerable<object[]> InvalidData => new List<object[]>
+    {
+        new object[] { -1, 0, 0 },
+        new object[] { 256, 0, 0 },
+        new object[] { 0, -1, 0 },
+        new object[] { 0, 256, 0 },
+        new object[] { 0, 0, -1 },
+        new object[] { 0, 0, 256 }
+    };
+
+    public static IEnumerable<object[]> ValidData => new List<object[]>
+    {
+        new object[] { 0, 0, 0 },
+        new object[] { 255, 0, 0 },
+        new object[] { 0, 0, 0 },
+        new object[] { 0, 255, 0 },
+        new object[] { 0, 0, 0 },
+        new object[] { 0, 0, 255 }
+    };
+}
