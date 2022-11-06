f8ce70b43942d2f8a19470902bc561b4dec02bf2
Javier Ramos Nodal <javiramos@soliss.com>
2022-11-06 21:00:20 +0100

    Renombro carpetas de précticas-

---
+++ Práctica_01/test/xUnit_Practicas1Tests/FactoriaDeColoresTests.cs
@@ -0,0 +1,69 @@
+using System.Drawing;
+using xUnit_Practicas1;
+using xUnit_Practicas1Tests.DataAttributes;
+using xUnit_Practicas1Tests.InputTestsData;
+
+namespace xUnit_Practicas1Tests;
+
+public class FactoriaDeColoresTests
+{
+    [Theory]
+    [ClassData(typeof(FactoriaDeColoresTestsData))]
+    public void GetColorByName_ShouldBeCorrectColor(Color expected, string colorName)
+    {
+        //Arrange
+        // string colorName = "";
+
+        //Act
+        var color = FactoriaDeColores.GetColorByName(colorName);
+
+        //Assert
+        Assert.Equal(expected, color);
+    }
+
+    [Theory]
+    [MemberData(nameof(FactoriaDeColoresGetColorCompositionTestsData.InvalidData), MemberType = typeof(FactoriaDeColoresGetColorCompositionTestsData))]
+    public void GetColorComposition_ShouldThrowArgumentOutOfRangeException_IfAnyNotIn0To255(int red, int green, int blue)
+    {
+        //Arrange
+        // string colorName = "";
+
+        //Act
+        Action act = () =>
+        {
+            var resultado = FactoriaDeColores.GetColorComposition(red, green, blue);
+        };
+
+        //Assert
+        Assert.Throws<ArgumentOutOfRangeException>(act);
+    }
+
+    [Theory]
+    [MemberData(nameof(FactoriaDeColoresGetColorCompositionTestsData.ValidData), MemberType = typeof(FactoriaDeColoresGetColorCompositionTestsData))]
+    public void GetColorComposition_ShouldBeCorrectColor_IfAllIn0To255(int red, int green, int blue)
+    {
+        //Arrange
+
+        //Act
+        var color = FactoriaDeColores.GetColorComposition(red, green, blue);
+
+        //Assert
+        Assert.True(red == color.R && green == color.G && blue == color.B);
+        //Assert.Equal(red, color.R);
+        //Assert.Equal(green, color.G);
+        //Assert.Equal(blue, color.A);
+    }
+
+    [Theory]
+    [JsonData(@"InputTestsData/InputData.json")]
+    public void ObtenerMensaje_ShouldBeCorrectMessage_JsonData(Color expected, int r, int g, int b)
+    {
+        //Arrange
+
+        //Act
+        var resultado = FactoriaDeColores.GetColorComposition(r, g, b);
+
+        //Assert
+        Assert.Equal(expected, resultado);
+    }
+}
