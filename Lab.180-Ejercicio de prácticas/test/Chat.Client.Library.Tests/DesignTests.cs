d1a136bdec775d8f759c96f29b82a6ce76ce2bb9
Javier Ramos Nodal <javiramos@soliss.com>
2022-12-06 12:18:51 +0100

    Prácticas propuestas para el módulo Fluent Assertions.

---
+++ Lab.180-Ejercicio de prácticas/test/Chat.Client.Library.Tests/DesignTests.cs
@@ -0,0 +1,67 @@
+﻿using Chat.Client.Library.Services;
+using Chat.Common.Entities;
+using Chat.Server.Library.Data;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Chat.Client.Library.Tests;
+
+public class DesignTests
+{
+    [Fact]
+    public void ChatClientLibrary_ShouldReferenceChatCommon()
+    {
+        //Arrange
+        var chatClientLibraryAssembly = typeof(IChatClient).Assembly;
+        var chatCommonAssembly = typeof(ChatUser).Assembly;
+
+        //Act
+
+        //Assert
+        chatClientLibraryAssembly.Should().Reference(chatCommonAssembly);
+    }
+
+    [Fact]
+    public void ChatClientLibrary_ShouldNotReferenceChatClient()
+    {
+        //Arrange
+        var chatClientLibraryAssembly = typeof(IChatClient).Assembly;
+        var chatClientAssembly = typeof(Program).Assembly;
+
+        //Act
+
+        //Assert
+        chatClientLibraryAssembly.Should().NotReference(chatClientAssembly);
+    }
+
+    [Fact]
+    public void ChatClientLibrary_ShouldNotReferenceChatServer()
+    {
+        //Arrange
+        var chatClientLibraryAssembly = typeof(IChatClient).Assembly;
+        var chatServerAssembly = typeof(Server.Program).Assembly;
+
+        //Act
+
+        //Assert
+        chatClientLibraryAssembly.Should().NotReference(chatServerAssembly);
+    }
+
+    [Fact]
+    public void ChatClientLibrary_ShouldNotReferenceChatServerLibrary()
+    {
+        //Arrange
+        var chatClientLibraryAssembly = typeof(IChatClient).Assembly;
+        var chatServerLibraryAssembly = typeof(ChatDbContext).Assembly;
+
+        //Act
+
+        //Assert
+        chatClientLibraryAssembly.Should().NotReference(chatServerLibraryAssembly);
+    }
+}
