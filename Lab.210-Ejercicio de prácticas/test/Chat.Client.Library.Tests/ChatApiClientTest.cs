e615717c77ec4663c95773be584b3ef85dad8cf4
Javier Ramos Nodal <javiramos@soliss.com>
2022-12-11 20:30:11 +0100

    pruebas HttpClient.

---
+++ Lab.210-Ejercicio de prácticas/test/Chat.Client.Library.Tests/ChatApiClientTest.cs
@@ -0,0 +1,63 @@
+﻿using Chat.Client.Library.Services;
+using Chat.Client.Library.Tests.TestData;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Chat.Client.Library.Tests;
+
+public class ChatApiClientTest
+{
+    private readonly ChatApiClient _chatApiClient;
+
+    public ChatApiClientTest()
+    {
+        var client = new HttpClient(new FakeMessageHandler());
+        client.BaseAddress = new Uri("http://localhost:5000/api/");
+        _chatApiClient = new ChatApiClient(client);
+    }
+
+    [Fact]
+    public async Task GetMessages_ShouldBeExpectedMessages()
+    {
+        //Arrange
+
+        //Act
+        var result = await _chatApiClient.GetChatMessagesAsync();
+
+        //Assert
+        result.Should().HaveCount(3);
+    }
+}
+
+public class FakeMessageHandler : HttpMessageHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        HttpResponseMessage response = null;
+
+        if (request.Method == HttpMethod.Post)
+        {
+            response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.Created,
+            };
+        }
+        else
+        {
+            response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(JsonConvert.SerializeObject(ChatMessageHelperData.ChatMessages), Encoding.UTF8, "application/json")
+            };
+        }
+
+        return Task.FromResult(response);
+    }
+}
---
+++ Lab.210-Ejercicio de prácticas/test/Chat.Client.Library.Tests/TestData/ChatMessageHelperData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Chat.Common.Entities;
+using Xunit;
+
+namespace Chat.Client.Library.Tests.TestData
+{
+    public class ChatMessageHelperData : TheoryData<ChatMessage, string>
+    {
+        public static List<ChatMessage> ChatMessages;
+
+        public ChatMessageHelperData()
+        {
+            var date = DateTime.Now;
+
+            ChatMessages = new List<ChatMessage>
+            {
+                new ChatMessage { Author = "Author1", Date = date, Message = "Hello everybody!" },
+                new ChatMessage { Author = "Author2", Date = date.AddMinutes(1), Message = "Hello Author1" },
+                new ChatMessage { Author = "Author1", Date = date.AddMinutes(2), Message = "How are you?" }
+            };
+
+            foreach (var chatMessage in ChatMessages)
+            {
+                Add(chatMessage, $"{chatMessage.Date:HH mm ss}:{chatMessage.Author}->{chatMessage.Message}");
+            }
+        }
+    }
+}
